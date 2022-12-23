f9ed541879bb765c3392d06c4982a077268bcd6d
IshanRastogi2608 <ishanrastogi26@gamil.com>
2022-12-23 21:02:21 +0530

    Fixed Mouse Tracking

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ const ctx = canvas.getContext("2d");
 
 // Some default values that we begin with
 
-let prevX = 0;
-let prevY = 0;
+let prevX = null;
+let prevY = null;
 let current_X = 0;
 let current_Y = 0;
 let is_drawing = false;
+
 
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 2;
@@ -37,7 +38,6 @@ color.addEventListener('change', (e)=>{
 // functionality to add background color to canvas
 canvas_bg_color.addEventListener('change', ()=>{
     canvas.style.backgroundColor=`${canvas_bg_color.value}`;
-    // console.log(canvas_bg_color.value)
 })
 
 // Functionality of our Width Controller Buttons
@@ -54,10 +54,10 @@ dec_btn.addEventListener('click', ()=>{
 
 // Drawing on our Canvas
 
-canvas.addEventListener("click", (e)=>{
-    console.log(e.offsetX, e.offsetY)
-    console.log(canvas.width, canvas.height)
-})
+// canvas.addEventListener("click", (e)=>{
+//     console.log(e.offsetX, e.offsetY)
+//     console.log(canvas.width, canvas.height)
+// })
 
 // 1. first see if we should draw or not
 canvas.addEventListener("mousedown", (e) => is_drawing = true);
@@ -67,36 +67,33 @@ canvas.addEventListener("mouseup", (e) => is_drawing = false);
 // 2. now make a line wherever the mouse goes
 canvas.addEventListener("mousemove", function(e){
 
-    // console.log("Mouse X: " + e.offsetX);
-    // console.log("Mouse Y: " + e.offsetY);
+    // var rect = e.target.getBoundingClientRect();
+    // var x = e.offsetX - rect.left; //x position within the element.
+    // var y = e.offsetY - rect.top;  //y position within the element.
 
+    mouse_coords = getMousePos(canvas, e)
 
-    if(prevX == 0 || prevY == 0 || !is_drawing){
-        prevX = e.offsetX;
-        prevY = e.offsetY;
+    if(prevX == null || prevY == null || !is_drawing){
+        // prevX = e.offsetX;
+        // prevY = e.offsetY;
+        prevX = mouse_coords.x;
+        prevY = mouse_coords.y;
         return;
     }
-    
-    // var rect = e.target.getBoundingClientRect();
-    // var x = e.clientX - rect.left; //x position within the element.
-    // var y = e.clientY - rect.top;  //y position within the element.
-   
-    current_X = e.offsetX;
-    current_Y = e.offsetY;
-    
 
-    console.log("Prev X is: "+prevX);
-    console.log("Prev Y is: "+prevY);
-    console.log("Current X: "+ current_X);
-    console.log("Current Y: "+ current_Y);
+    // current_X = e.offsetX;
+    // current_Y = e.offsetY;
+    
+    current_X = mouse_coords.x;
+    current_Y = mouse_coords.y;
 
     ctx.beginPath();
     ctx.moveTo(prevX, prevY);
     ctx.lineTo(current_X, current_Y);
     ctx.stroke();
 
-    prevX = e.offsetX;
-    prevY = e.offsetY;
+    prevX = current_X;
+    prevY = current_Y;
 })
 
 
@@ -110,6 +107,18 @@ download_btn.addEventListener("click", () => {
     let data = canvas.toDataURL("image/png")
     let a = document.createElement("a")
     a.href = data
-    a.download = "sketch.png"
+    a.download = "drawing.png"
     a.click()
 })
+
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect(), 
+      scaleX = canvas.width / rect.width, 
+      scaleY = canvas.height / rect.height;
+  
+    return {
+      x: (evt.clientX - rect.left) * scaleX,
+      y: (evt.clientY - rect.top) * scaleY
+    }
+  }
